ea6097fea96a98544f47d2c7172f7ccbd764cee9
jboffa03 <102766471+jboffa03@users.noreply.github.com>
2023-04-16 15:55:57 -0400

    Drinks and Order History added

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -28,15 +28,16 @@ def create_app():
     @app.route("/")
     def welcome():
         return "<h1>Welcome to the 3200 boilerplate app</h1>"
+    
 
     # Import the various routes
     from src.views import views
     from src.customers.customers import customers
-    from src.products.products  import products
+    from src.drinks.drinks  import drinks
 
     # Register the routes that we just imported so they can be properly handled
     app.register_blueprint(views,       url_prefix='/v')
     app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(drinks,    url_prefix='/d')
 
     return app
--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -34,3 +34,23 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Get the customer's order history
+@customers.route('/orders/<userID>', methods=['GET'])
+def get_order_history(userID):
+    cursor = db.get_db().cursor()
+    query = '''
+        SELECT order_date, order_id, order_method, order_name, price, user_id
+        FROM Orders NATURAL JOIN Drink
+        where user_id = {0}'''.format(userID)
+       
+    cursor.execute(query)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
