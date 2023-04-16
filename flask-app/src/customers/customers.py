905c7503e3fc9b55d1167087ffefde7b2e2a6bb0
jboffa03 <102766471+jboffa03@users.noreply.github.com>
2023-04-16 18:59:26 -0400

    semicolon

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
+from datetime import date
 
 
 customers = Blueprint('customers', __name__)
@@ -80,3 +81,33 @@ def get_order_history(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+"""
+#Customers can place online orders
+@customers.route('/placeOrder', method = ['PUT'])
+def place_order():
+    the_data = request.json
+    current_app.logger.info(the_data)
+    cursor = db.get_db().cursor()
+    
+
+    order_name = the_data['drink_name']
+    order_date = date.today()
+    order_method = 'Online'
+    user_id = the_data['user_id']
+    cursor.execute('SELECT max(order_id) from Order')
+    order_id = cursor.fetchall()
+    order_id = int((order_id[0])[0])+1
+
+    query = 'INSERT INTO Order (order_name, order_date, user_id, ) values ("'
+    query += cst_name +'", '
+    query += "STR_TO_DATE('" + birthday + "' ,'%d,%m,%Y'), \""
+    query += str(user_id) +'")'
+    current_app.logger.info(query)
+
+    cursor2 = db.get_db().cursor()
+    cursor2.execute(query)
+    db.get_db().commit()
+    return 'New customer:' + cst_name +', ' + birthday +', ' + str(user_id) +'.'
+    """
