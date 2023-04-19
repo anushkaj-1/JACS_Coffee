d0cdb3925953aefb1e3f6aaf4b85245208e906a5
jboffa03 <102766471+jboffa03@users.noreply.github.com>
2023-04-19 19:07:11 -0400

    Reformatted outputs

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -137,15 +137,8 @@ def place_order():
 def get_points(userID):
     cursor = db.get_db().cursor()
     cursor.execute('select points from Customer where user_id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+    the_response = cursor.fetchall()
+    return str(the_response[0][0])
 
 #Access customers rewards
 @customers.route('/rewards/<userID>', methods = ['GET', 'POST'])
