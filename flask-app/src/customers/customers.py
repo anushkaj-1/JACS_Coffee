25692709c37081367a5e32f71b71d8046f1dbf1a
jboffa03 <102766471+jboffa03@users.noreply.github.com>
2023-04-19 20:22:17 -0400

    added get all rewards

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -191,3 +191,29 @@ def cst_rewards(userID):
         cursor3.execute(query)
         db.get_db().commit()
         return 'New reward:' + discount +', ' + exp_date +', ' + item + ', ' + user_id + ', ' + pointValue + ', ' + str(reward_id) +'.'
+    
+    #gets all rewards in database
+@customers.route('/allRewards', methods=['GET'])
+def get_all_reward():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT * from Rewards')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
