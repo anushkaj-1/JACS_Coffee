60707ee57e122217f352a35e410253e385d6025d
anushkaj-1 <125081503+anushkaj-1@users.noreply.github.com>
2023-04-20 00:30:07 -0400

    Update employees.py
    better route

--- flask-app/src/employees/employees.py
+++ flask-app/src/employees/employees.py
@@ -58,7 +58,7 @@ def get_order(method):
 def get_orders():
     cursor = db.get_db().cursor()
     query = '''
-        SELECT size, type, milk, topping, cst_name 
+        SELECT size, type, milk, topping, cst_name, order_id 
         FROM Drnk_Ord JOIN Drink USING (drink_id) JOIN 
         Orders USING (order_id) JOIN Toppings USING (order_id) 
         JOIN Customer USING (user_id)
@@ -145,7 +145,7 @@ def get_order_detail(orderID):
 
 
     # constuct statement
-    quesry = '''SELECT size, milk, type, drink_id, COUNT(drink_id)
+    query = '''SELECT size, milk, type, drink_id, COUNT(drink_id)
 FROM (
     SELECT D2.drink_id as drink_id, size, milk, type, O.order_id
     FROM Orders O JOIN Drnk_Ord D on O.order_id = D.order_id JOIN Drink D2 on D.drink_id = D2.drink_id
